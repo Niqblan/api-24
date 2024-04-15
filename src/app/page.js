86066352de9174128c0d99785c21c4f29ac7a597
Niqblan <77024809+Niqblan@users.noreply.github.com>
2024-04-15 19:59:56 -0300

    sin margen

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,16 @@
 
 export default function Home() {
   return (
-    <div>
-  <h1 className="font-semibold text-[40px] mt-48 text-center">
+    <div className="h-[100vh] flex flex-col justify-center gap-10 bg-black text-white">
+  <h1 className="font-semibold text-[40px] text-center">
     Bienvenidos al sitio de Recomendación de películas!
   </h1>
 
-  <main className="flex min-h-screen flex-col items-center justify-center">
+  <main className="flex flex-col items-center justify-center">
     <div className="grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:text-center ">
       <a
         href="/signUp"
-        className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        className="group rounded-lg border border-transparent px-5 py-4 transition-colors  hover:border-neutral-700 hover:bg-neutral-800/30"
       >
         <h2 className={` text-2xl font-semibold `}>
           ¡Comencemos!
