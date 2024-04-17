3e08a1427bda3f1035aa6549ad038e9bf6253261
Emiliano55 <Emiliano55>
2024-04-16 23:46:16 -0300

    fondo ajuste de slides

--- src/app/page.js
+++ src/app/page.js
@@ -2,15 +2,13 @@
 import React from 'react';
 import SliderComponent from "@/components/SliderComponent";
 import './globals.css';
-// import function to register Swiper custom elements
 import { register } from "swiper/element/bundle";
-// register Swiper custom elements
 register();
 
 
 export default function Home() {
   return (
-    <div className="h-[1200px] flex flex-col justify-center gap-10 bg-black text-white ">
+    <div className="h-[1500px] flex flex-col justify-center gap-10 text-white "style={{ marginLeft: '5px'}}>
       <div className='flex flex-col gap-2 h-[200px]'>
       <h1 className="titulo">Pelis</h1>
       <SliderComponent />
