8c975e2c87617eacf4545ea957dadaa29eef9b9c
Emiliano55 <Emiliano55>
2024-04-17 12:12:40 -0300

    arreglo de slide

--- src/app/page.js
+++ src/app/page.js
@@ -8,20 +8,20 @@ register();
 
 export default function Home() {
   return (
-    <div className="h-[1500px] flex flex-col justify-center gap-10 text-white "style={{ marginLeft: '5px'}}>
-      <div className='flex flex-col gap-2 h-[200px]'>
+    <div className="h-[1300px] flex flex-col w-full justify-center gap-10 text-white "style={{ marginLeft: '5px'}}>
+      <div className='flex flex-col gap-2 h-[200px] w-full'>
       <h1 className="titulo">Pelis</h1>
       <SliderComponent />
       </div>
-      <div className='flex flex-col gap-2 h-[200px]'>
+      <div className='flex flex-col gap-2 h-[200px] w-full'>
       <h1 className="titulo">Pelis</h1>
       <SliderComponent />
       </div>
-      <div className='flex flex-col gap-2 h-[200px]'>
+      <div className='flex flex-col gap-2 h-[200px] w-full'>
       <h1 className="titulo">Pelis</h1>
       <SliderComponent />
       </div>
-      <div className='flex flex-col gap-2 h-[200px]'>
+      <div className='flex flex-col gap-2 h-[200px] w-full'>
       <h1 className="titulo">Pelis</h1>
       <SliderComponent />
       </div>
