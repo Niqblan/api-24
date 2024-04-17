349d63375be7b286cef34a3a6be4d11b662834e0
Emiliano55 <Emiliano55>
2024-04-17 13:35:24 -0300

    titulos, modificaciones en slides

--- src/components/SliderComponent.js
+++ src/components/SliderComponent.js
@@ -6,11 +6,15 @@ import './SliderComponent.css'
     
   return(
   <div className='container2'>
-    <swiper-container loop={true} space-between='4' slides-per-view="5"
+    <swiper-container loop={true} space-between='4' slides-per-view="6"
         navigation="true"
         style={{
           "--swiper-navigation-color": "#fff",
-          "--swiper-pagination-color": "#fff"
+          "--swiper-pagination-color": "#fff",
+          "--swiper-navigation-size": "30px",
+          "--swiper-navigation-top-offset": "50%",
+          "--swiper-navigation-sides-offset": "10px",
+          "--swiper-navigation-color": "var(--swiper-theme-color)"
         }}>
       <swiper-slide>slide1</swiper-slide>
       <swiper-slide>slide</swiper-slide>
