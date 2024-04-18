4d30955a2cc6c432f44ea6b8b924f969f8dd023a
Niqblan <nicoblanco@uade.edu.ar>
2024-04-18 00:28:23 -0300

    boton

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,9 +7,9 @@ export default function Header(){
     <div className="bg-black w-full py-4 px-6 flex items-center justify-between">
       <h1 className="text-2xl font-bold" style={{ backgroundImage: 'linear-gradient(163deg, #00ff75 0%, #3700ff 100%)', WebkitBackgroundClip: 'text', WebkitTextFillColor: 'transparent' }}>Esto Es Cine</h1>
     
-      <div className='flex gap-4'>
-        <a href='./signIn' className='text-white' > Inicia Sesión! </a>
-        <a href='./signUp' className='text-white' > Registrate! </a>
+      <div>
+        <a href='./signIn' className='text-white hover:bg-white hover:text-black hover:rounded-xl px-3 py-1 ' > Inicia Sesión! </a>
+        <a href='./signUp' className='text-white hover:bg-white hover:text-black hover:rounded-xl px-3 py-1  ' > Registrate! </a>
       </div>
       
     </div>
